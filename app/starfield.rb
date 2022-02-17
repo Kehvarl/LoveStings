d235131830b55df97a6a68991c1fac1209f195e4
Kehvarl <Kehvarl@Kehvarl.com>
2022-02-16 17:56:58 -0700

    playing with stars again

--- app/starfield.rb
+++ app/starfield.rb
@@ -1,8 +1,26 @@
 class Star
   attr_accessor :x, :y, :z, :sh, :r, :c, :mc
-  def initialize x=1280, mx = 0
-    @x = rand(x) + mx
-    @y = rand(720)
+  def initialize vx=0, vy=0
+    if vy == 0
+      @x = rand(1280)
+    else
+      if vx > 0
+        @x = 1280
+      else
+        @x = 0
+      end
+    end
+
+    if @x >= 1270 or vy == 0
+      @y = rand(720)
+    else
+      if vy < 0
+        @y = 720
+      else
+        @y = 0
+      end
+
+    end
     @z = rand(127) + 1
     @sh = rand(12) + 4
     @r = rand(360)
@@ -35,16 +53,16 @@ class Starfield
   def initialize
     @stars = []
     0..128.each { @stars << Star.new() }
-    @x = 0
     @vx = -0.1
+    @vy = -0.1
   end
 
   def reverse
     @vx = -@vx
+    @vy = -@vy
   end
 
   def tick
-    @x += @vx
     # @stars.each { |s| s.calc() }
   end
 
@@ -54,16 +72,16 @@ class Starfield
     stars = 0
     @stars.each do |s|
       s.x += (@vx * s.z)
+      s.y += (@vy * s.z)
       if s.x >= 0 and s.x <= 1280
         arr << s.render()
         stars += 1
       end
     end
     if stars < 128
-      if @vx < 0
-        1..(128 - stars).each {@stars << Star.new(1280, 1280)}
-      else
-        1..(128 - stars).each {@stars << Star.new(0, 0)}
+      1..(128 - stars).each do
+        @stars << Star.new(@vx, 0)
+        @stars << Star.new(0, @vy)
       end
     end
     arr
