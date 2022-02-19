c70c91230c7143ee83a8b960d4ef04e911243fd7
Kehvarl <Kehvarl@Kehvarl.com>
2022-02-18 19:32:15 -0700

    thrust indicators

--- app/tick.rb
+++ app/tick.rb
@@ -2,6 +2,7 @@ def tick args
   args.state.starfield ||= Starfield.new()
   args.state.ship_y ||= 360
   args.state.ship_angle ||= 270
+  tpath = false
 
   if args.inputs.mouse.click
     args.state.starfield.reverse()
@@ -10,14 +11,18 @@ def tick args
 
   if args.inputs.keyboard.left
     args.state.starfield.vx += 0.01
+    tpath = 'sprites/ThrustB.png'
   elsif args.inputs.keyboard.right
     args.state.starfield.vx -= 0.01
+    tpath = 'sprites/ThrustF.png'
   end
 
   if args.inputs.keyboard.up
     args.state.ship_y += 1
+    tpath = 'sprites/ThrustR.png'
   elsif args.inputs.keyboard.down
     args.state.ship_y -= 1
+    tpath = 'sprites/ThrustL.png'
   end
 
 
@@ -30,5 +35,8 @@ def tick args
   args.state.starfield.tick()
 
   args.outputs.primitives << args.state.starfield.render()
-  args.outputs.primitives << {x: 640, y: args.state.ship_y, w:48, h:48, path:'sprites/ship_1.png', angle: args.state.ship_angle}.sprite!
+  args.outputs.primitives << {x: 640, y: args.state.ship_y, w:48, h:48, path:'sprites/Ship.png', angle: args.state.ship_angle}.sprite!
+  if tpath
+    args.outputs.primitives << {x: 640, y: args.state.ship_y, w:48, h:48, path:tpath, angle: 270}.sprite!
+  end
 end
