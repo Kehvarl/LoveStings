2d14dc904d5705d4fca5e9d8c7a77b5ffa808470
Kehvarl <Kehvarl@Kehvarl.com>
2022-02-25 22:36:44 -0700

    colors and multi-line layout

--- app/tick.rb
+++ app/tick.rb
@@ -19,15 +19,23 @@ def tick args
   args.outputs.primitives << args.state.ship
   args.outputs.primitives << args.state.ship.render
 
-  args.outputs.primitives << [240, 0, 800, 100, 64, 64, 64].borders
-  args.outputs.primitives << [241, 1, 798, 98, 128, 128, 128].borders
-  args.outputs.primitives << [242, 2, 796, 96, 255, 255, 255].borders
-  args.outputs.primitives << [243, 3, 794, 94, 128, 128, 128].borders
-  args.outputs.primitives << [244, 4, 792, 92, 64, 64, 64].borders
-  args.outputs.primitives << [245, 5, 790, 90, 0, 0, 0].solids
+  args.outputs.primitives << [240, 0, 800, 100, 64,  64,  64].borders
+  args.outputs.primitives << [241, 1, 798,  98,  0, 128, 128].borders
+  args.outputs.primitives << [242, 2, 796,  96,  0, 255, 255].borders
+  args.outputs.primitives << [243, 3, 794,  94,  0, 128, 128].borders
+  args.outputs.primitives << [244, 4, 792,  92, 64,  64,  64].borders
+  args.outputs.primitives << [245, 5, 790,  90,  0,   0,   0].solids
   args.outputs.primitives << {x: 245, y: 80, size_enum: 2,
                               r: 255, g: 255, b: 255,
                               text: "Testing test text that needs tested...",
                               alignment_enum: 0, vertical_alignment_enum: 1}.label!
 
+  long_string = "Lorem ipsum dolor sit amet, consectetur adipiscing elitteger dolor velit, ultricies vitae libero vel, aliquam imperdiet enim."
+  max_character_length = 70
+  long_strings_split = args.string.wrapped_lines long_string, max_character_length
+  args.outputs.primitives << long_strings_split.map_with_index do |s, i|
+    { x: 245, y: 55 - (i * 25), size_enum: 2, text: s,
+      r: 255, g: 255, b: 255,
+      alignment_enum: 0, vertical_alignment_enum: 1}.label!
+  end
 end
