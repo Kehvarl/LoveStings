5de82cb2dc94f5b71134bec4a9ab86b87f33c7c8
Kehvarl <Kehvarl@Kehvarl.com>
2022-03-02 18:46:34 -0700

    toast collecting, and scorekeeping

--- app/minigames/dodger.rb
+++ app/minigames/dodger.rb
@@ -10,6 +10,8 @@ class Minigame_Dodger
     @background = []
     @effects = []
     @next_enemy = 60
+    @lives = 3
+    @score = 0
     while @enemies.length < 5
       new_enemy
     end
@@ -48,7 +50,7 @@ class Minigame_Dodger
 
   def new_toast
     s = [16, 32].sample
-    t = RotatingSprite.new(x: rand(64) + 1280, y: rand(720) - s,
+    t = RotatingSprite.new(x: rand(64) + 1280, y: rand(670 - s -s) + s,
                            w: s, h: s, angle: rand(360),
                            rotation: 3,
                            vx: rand(10)/10,
@@ -84,7 +86,7 @@ class Minigame_Dodger
 
   def new_enemy
     s = [16, 32, 64].sample
-    e =  RotatingSprite.new(x: rand(64) + 1280, y: rand(688),
+    e =  RotatingSprite.new(x: rand(64) + 1280, y: rand(670 -s -s) + s,
                             w: s,
                             h: s, angle: rand(360),
                             rotation: [-6, -4, -2, -1, 1, 2, 3, 4, 6].sample,
@@ -118,7 +120,9 @@ class Minigame_Dodger
     @next_enemy -= 1
     if @next_enemy == 0
       new_enemy
-      new_toast
+      if rand(10) >= 7
+        new_toast
+      end
       @next_enemy = 60
     end
     if @ship.exists
@@ -145,11 +149,39 @@ class Minigame_Dodger
       if @ship.exists and r.intersect_rect?(@ship.rect)
         @effects << Explosion.new(x: @ship.x, y: @ship.y)
         @ship.exists = false
+        @lives -= 1
         @ship.vx = @ship.vy = 0
         {x:r[0], y:r[1], w:r[2], h:r[3], r:128, g:0, b:0, angle: e.angle}.border!
       end
     end
+
+    args.outputs.primitives << @toast.map do |e|
+      r = e.rect
+      if @ship.exists and r.intersect_rect?(@ship.rect)
+        @score += 100
+        e.exists = false
+        {x:r[0], y:r[1], w:r[2], h:r[3], r:0, g:128, b:0, angle: e.angle}.border!
+      end
+    end
+    @toast = @toast.select { |t| t.exists}
+
     args.outputs.primitives << @effects.map { |e|  e}
+
+    args.outputs.primitives << {x: 100, y: 689, w: 20, h: 20, path: 'sprites/toast.png'}.sprite!
+    args.outputs.primitives << {x: 120, y: 710,
+                                text: ": #{@score.to_s.rjust(3, '0')}",
+                                size_enum: 2,
+                                r:0, g:255, b:0}.label!
+    args.outputs.primitives << {x: 1050, y: 683, w: 32, h: 32, path: 'sprites/Ship.png'}.sprite!
+    args.outputs.primitives << {x: 1080, y: 710,
+                                text: ": #{@lives}",
+                                size_enum: 2,
+                                r:0, g:255, b:0}.label!
+    args.outputs.primitives << {x: 0, y: 675, x2: 190, y2: 675, r: 0, g: 128, b: 0}.line!
+    args.outputs.primitives << {x: 190, y: 675, x2: 210, y2: 715, r: 0, g: 128, b: 0}.line!
+    args.outputs.primitives << {x: 210, y: 715, x2: 1010, y2: 715, r: 0, g: 128, b: 0}.line!
+    args.outputs.primitives << {x: 1010, y: 715, x2: 1030, y2: 675, r: 0, g: 128, b: 0}.line!
+    args.outputs.primitives << {x: 1030, y: 675, x2: 1280, y2: 675, r: 0, g: 128, b: 0}.line!
     #args.outputs.primitives << @background.map { |g|  g}
 
     #@background = ground_tick
